7728f63fa6dff365e9dc0a55dcaeb846dc60769f
lkkl5885 <khb12347@naver.com>
2016-10-03 20:55:43 +0900

    HARITA_Index
    HARITA_Index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./mainStyle.css">
+    <title></title>
+  </head>
+  <body style="margin:0px;">
+
+    <div class="" id = "bg">
+
+      <ul class="topNav" id = "topNav">
+        <li><a href="/Gyeonggido.html">경기도</a></li>
+        <li><a href="#">강원도</a></li>
+        <li><a href="#">특별광역시</a></li>
+        <li><a href="#">전라도</a></li>
+        <li><a href="#">경상도</a></li>
+        <li><a href="#">충청도</a></li>
+      </ul>
+
+      <div>
+       <p>
+         <i>
+         <a class="mainTitle" href="#">HARITA<a/>
+           </i>
+       </p>
+
+      </div>
+
+    </div>
+
+
+
+  </body>
+</html>
